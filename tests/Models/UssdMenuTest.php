10b46671843ba7a61fea369c4672ed4189a36b41
KAMARO <kamaroly@gmail.com>
2020-12-01 11:32:03 +0300

    Added parent and children retrieval

--- tests/Models/UssdMenuTest.php
+++ tests/Models/UssdMenuTest.php
@@ -23,10 +23,32 @@ class UssdMenuTest extends TestCase
     	$this->assertEquals('Home', $subMenu->parent_name);
     }
 
-    /**@test*/
+    /**   @test */
     public function test_it_returns_root_when_menu_does_not_have_parent($value='')
     {
     	$menu = UssdMenu::create(['name' => 'Home', 'parent_id' => 0]);
     	$this->assertEquals('Root', $menu->parent_name);
+    }
+
+    /**   @test */
+    public function it_should_retrieve_children()
+    {
+    	$menu = UssdMenu::create(['name' => 'Home', 'parent_id' => 0]);
+    	UssdMenu::create(['name' => 'Sub-menu-1', 'parent_id' => 1]);
+    	UssdMenu::create(['name' => 'Sub-menu-2', 'parent_id' => 1]);
+    	UssdMenu::create(['name' => 'Sub-menu-3', 'parent_id' => 1]);
+
+    	$this->assertEquals(3, $menu->children->count());
+    }
+
+	/**   @test */
+    public function it_should_retrieve_parent()
+    {
+    	$menu = UssdMenu::create(['name' => 'Home', 'parent_id' => 0]);
+    	UssdMenu::create(['name' => 'Sub-menu-1', 'parent_id' => 1]);
+    	UssdMenu::create(['name' => 'Sub-menu-2', 'parent_id' => 1]);
+    	$submenu = UssdMenu::create(['name' => 'Sub-menu-3', 'parent_id' => 1]);
+
+    	$this->assertEquals(1, $submenu->parent->id);
     }
 }
